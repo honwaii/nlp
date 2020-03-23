8731f4bea0511b0ea492e9cddfd2b1c899c59d1d
lihongwei <ys.lihongwei@h3c.com>
2020-03-23 14:39:50 +0800

    update.

--- assignment07/recurrent_neural_networks.py
+++ assignment07/recurrent_neural_networks.py
@@ -8,18 +8,18 @@ from io import open
 import glob
 import os
 import matplotlib.pyplot as plt
-
+import random
 import unicodedata
 import string
 import torch
-
+import time
+import math
 from assignment07.rnn import RNN
+import torch.nn as nn
 
 
 def find_files(path): return glob.glob(path)
 
-
-print(find_files('data/names/*.txt'))
 
 all_letters = string.ascii_letters + " .,;'"
 n_letters = len(all_letters)
@@ -34,12 +34,7 @@ def unicode_2_Ascii(s):
     )
 
 
-print(unicode_2_Ascii('Ślusàrski'))
-
-
 # Build the category_lines dictionary, a list of names per language
-
-
 # Read a file and split into lines
 def read_lines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
@@ -80,62 +75,41 @@ def line_to_tensor(line):
     return tensor
 
 
-def category_from_output(output):
+def category_from_output(output, all_categories: list):
     top_n, top_i = output.topk(1)
     category_i = top_i[0].item()
     return all_categories[category_i], category_i
-
-
-n_hidden = 128
-all_categories, category_lines = get_languages_and_names()
-n_categories = len(all_categories)
-rnn = RNN(n_letters, n_hidden, n_categories)
-print(line_to_tensor('Jones'))
-input = letter_to_tensor('A')
-hidden = torch.zeros(1, n_hidden)
-
-output, next_hidden = rnn(input, hidden)
-
-input = line_to_tensor('Albert')
-hidden = torch.zeros(1, n_hidden)
-
-output, next_hidden = rnn(input[0], hidden)
-print(output)
-print(category_from_output(output))
-
-import random
 
 
 def sample(l):
     return l[random.randint(0, len(l) - 1)]
 
 
-def sample_trainning():
-    category = sample(all_categories)
-    line = sample(category_lines[category])
+def sample_training(all_categories: list, category_lines: dict):
+    category = sample(all_categories)  # all_categories:所有的国家名 ，category: 某个国家名
+    line = sample(category_lines[category])  # line : 某个国家的名字
     category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
     line_tensor = line_to_tensor(line)
     return category, line, category_tensor, line_tensor
 
 
-for i in range(10):
-    category, line, category_tensor, line_tensor = sample_trainning()
-    print('category =', category, '/ line =', line)
-import torch.nn as nn
+# for i in range(10):
+#     category, line, category_tensor, line_tensor = sample_training()
+#     print('category =', category, '/ line =', line)
 
-criterion = nn.CrossEntropyLoss()
 
 learning_rate = 0.005  # If you set this too high, it might explode. If too low, it might not learn
 
 
-def train(category_tensor, line_tensor):
+def train(category_tensor, line_tensor, rnn: RNN):
     hidden = rnn.initHidden()
+    criterion = nn.CrossEntropyLoss()
 
     rnn.zero_grad()
-
+    output = None
     for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
-
+        output, hidden = rnn(line_tensor[i], hidden)  # 第i个字母的tensor
+    # 将所有的输入传入后，最后得到的输出，来比较loss
     loss = criterion(output, category_tensor)
     loss.backward()
 
@@ -146,16 +120,7 @@ def train(category_tensor, line_tensor):
     return output, loss.item()
 
 
-import time
-import math
-
-n_iters = 1000  # 这个数字你可以调大一些
-print_every = 500
-plot_every = 100
-
 # Keep track of losses for plotting
-current_loss = 0
-all_losses = []
 
 
 def time_since(since):
@@ -166,36 +131,40 @@ def time_since(since):
     return '%dm %ds' % (m, s)
 
 
-start = time.time()
-
-for iter in range(1, n_iters + 1):
-    category, line, category_tensor, line_tensor = sample_trainning()
-    output, loss = train(category_tensor, line_tensor)
-    current_loss += loss
-
-    # Print iter number, loss, name and guess
-    if iter % print_every == 0:
-        guess, guess_i = category_from_output(output)
-        correct = '✓' if guess == category else '✗ (%s)' % category
-        print('%d %d%% (%s) %.4f %s / %s %s' % (
-            iter, iter / n_iters * 100, time_since(start), loss, line, guess, correct))
-
-    # Add current loss avg to list of losses
-    if iter % plot_every == 0:
-        all_losses.append(current_loss / plot_every)
-        current_loss = 0
+current_loss = 0
+all_losses = []
 
 
-def evaluate(line_tensor):
+def training():
+    start = time.time()
+    for iter in range(1, n_iters + 1):
+        category, line, category_tensor, line_tensor = sample_training()
+        output, loss = train(category_tensor, line_tensor)
+        current_loss += loss
+
+        # Print iter number, loss, name and guess
+        if iter % print_every == 0:
+            guess, guess_i = category_from_output(output)
+            correct = '✓' if guess == category else '✗ (%s)' % category
+            print('%d %d%% (%s) %.4f %s / %s %s' % (
+                iter, iter / n_iters * 100, time_since(start), loss, line, guess, correct))
+
+        # Add current loss avg to list of losses
+        if iter % plot_every == 0:
+            all_losses.append(current_loss / plot_every)
+            current_loss = 0
+
+
+def evaluate(line_tensor, rnn: RNN):
     hidden = rnn.initHidden()
-
+    output = None
     for i in range(line_tensor.size()[0]):
         output, hidden = rnn(line_tensor[i], hidden)
 
     return output
 
 
-def predict(input_line, n_predictions=3):
+def predict(input_line, all_categories: list, n_predictions=3):
     print('\n> %s' % input_line)
     with torch.no_grad():
         output = evaluate(line_to_tensor(input_line))
@@ -211,6 +180,27 @@ def predict(input_line, n_predictions=3):
             predictions.append([value, all_categories[category_index]])
 
 
+n_hidden = 128
+# all_categories, category_lines = get_languages_and_names()
+# rnn = RNN(n_letters, n_hidden, len(all_categories))
+
+n_iters = 1000  # 这个数字你可以调大一些
+print_every = 500
+plot_every = 100
+
 predict('Dovesky')
 predict('Jackson')
 predict('Satoshi')
+
+# 代码练习
+# 1. 尝试在我们的RNN模型中添加更多layers，然后观察Loss变化
+# todo
+#
+# 2. 将原始的RNN模型改成nn.LSTM和nn.GRU， 并且改变 n_iters = 1000 这个值，观察其变化
+# todo
+#
+# 3. 把该RNN模型变成多层RNN模型，观察Loss的变化
+# todo
+#
+# 4. Pytorch里边常用nn.NLLoss来代替crossentropy，将criterion改为nn.NLLoss，观察变化
+# todo
