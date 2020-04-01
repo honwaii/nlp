9ffd2c8cf1e545d27aa095f5d2a445a91f87fe8f
honwaii <honwaii@126.com>
2020-04-02 07:41:22 +0800

    ....

--- assignment07/recurrent_neural_networks.py
+++ assignment07/recurrent_neural_networks.py
@@ -160,7 +160,50 @@ def training(all_categories: list, category_lines: dict, rnn):
         if iter % plot_every == 0:
             all_losses.append(current_loss / plot_every)
             current_loss = 0
+        loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
     return all_losses
+
+
+def lstm_train(rnntr55555):
+    start = time.time()
+
+    current_loss = 0
+    all_losses = []
+    criterion = nn.CrossEntropyLoss()
+
+    rnn.zero_grad()
+    output = None
+    for iter in range(1, n_iters + 1):
+        category, line, category_tensor, line_tensor = sample_training(all_categories, category_lines)
+        # output, loss = train(category_tensor, line_tensor, rnn)
+        print(line_tensor.shape)
+        output = rnn(line_tensor)
+        print(output.shape)
+        loss = criterion(output, category_tensor)
+        current_loss += loss
+
+        # Print iter number, loss, name and guess
+        if iter % print_every == 0:
+            guess, guess_i = category_from_output(output, all_categories)
+            correct = '✓' if guess == category else '✗ (%s)' % category
+            print('%d %d%% (%s) %.4f %s / %s %s' % (
+                iter, iter / n_iters * 100, time_since(start), loss, line, guess, correct))
+
+        # Add current loss avg to list of losses
+        if iter % plot_every == 0:
+            all_losses.append(current_loss / plot_every)
+            current_loss = 0
+        loss.backward()
+
+        # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+    return
 
 
 def evaluate(line_tensor, rnn: RNN):
