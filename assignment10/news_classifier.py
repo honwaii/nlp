4b596f6e6f30125939065f30a228ac40211f228d
honwaii <honwaii@126.com>
2020-04-16 00:56:54 +0800

    update.

--- assignment10/news_classifier.py
+++ assignment10/news_classifier.py
@@ -7,7 +7,10 @@
 
 import pandas as pd
 import jieba
-import time
+import numpy as  np
+from sklearn.decomposition import PCA
+from gensim.models import KeyedVectors
+from gensim.models.word2vec import Word2Vec
 
 
 class News:
@@ -19,31 +22,50 @@ class News:
 def handle_news():
     essays_path = './news_data.csv'
     contents = pd.read_csv(essays_path, encoding='gb18030', usecols=["source", "content"])
-    essays = []
-    count = 0
-    stop_words = open(u'中文停用词表.txt', "r", encoding="utf-8").readlines()
+    stop_words_1 = open(u'哈工大停用词表.txt', "r", encoding="utf-8").readlines()
+    stop_words_2 = open(u'中文停用词表.txt', "r", encoding="utf-8").readlines()
+    stop_words = stop_words_1 + stop_words_2
     stop_words_list = [line.strip() for line in stop_words]
+    news_xinhua = []
+    news_other = []
+    count = 0
     for each in contents.iterrows():
         content = str(each[1]['content']).strip()
         source = str(each[1]['source']).strip()
+        if content == 'nan':
+            continue
         if content is None or not isinstance(content, str):
             continue
-        content = split_content(content, stop_words_list)
+        content = content.replace("\n", "。").strip()
+        content = content.replace(r"\n", "。").strip()
+        content = content.replace("\r", "。").strip()
+        content = content.replace("\t", "。").strip()
+        content1 = content.replace("新华社", "").strip()
+        content = split_content(content1, stop_words_list) + "\n"
         if '新华社' in source:
-            news = News(content, 1)
-            count += 1
+            news_xinhua.append(content)
         else:
-            news = News(content, 0)
-        essays.append(news)
-        if count % 10000 == 0:
-            print(content)
-    print("获取到的文章数:" + str(len(essays)))
-    print("新华社的文章数:" + str(count))
-    return essays
+            news_other.append(content)
+        count += 1
+        if count % 1000 == 0:
+            print('others:' + str(len(news_other)))
+            print('新华社:' + str(len(news_xinhua)))
+    with open("./新华社新闻.txt", 'w', encoding='utf-8') as f:
+        f.writelines(news_xinhua)
+        f.flush()
+        f.close()
+    with open("./other_news.txt", 'w', encoding='utf-8') as f:
+        f.writelines(news_other)
+        f.flush()
+        f.close()
+    # print("获取到的文章数:" + str(len(essays)))
+    # print("新华社的文章数:" + str(count))
+    return
 
 
 def split_content(content: str, stop_words: list):
     simpled = ''
+    s = content.replace("新华社", "")
     s = content.replace("\n", "")
     if s == "":
         return simpled
@@ -55,4 +77,107 @@ def split_content(content: str, stop_words: list):
     return simpled
 
 
-handle_news()
+def get_words_frequency_dict(path: str):
+    print("load word frequency file from " + path)
+    word2weight = {}
+    with open(path, encoding='utf-8') as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if len(line) <= 0:
+            continue
+        line = line.split()
+        if len(line) == 2:
+            word2weight[line[0]] = float(line[1])
+        else:
+            print(line)
+    return word2weight
+
+
+def get_word_frequency(word_text, look_table):
+    if word_text in look_table:
+        return look_table[word_text]
+    else:
+        return 1.0
+
+
+def get_word_vector(word: str, word_vector_model: Word2Vec):
+    try:
+        word_vector = word_vector_model[word]
+    except KeyError:
+        word_vector = np.zeros(word_vector_model.vector_size)
+    return word_vector
+
+
+def sentence_to_vec(sentence_list: list, word_vec: Word2Vec, look_table: dict, a=1e-3):
+    sentence_set = []
+    for sentence in sentence_list:
+        vs = np.zeros(word_vec.vector_size)  # add all word2vec values into one vector for the sentence
+        sentence_length = sentence.len()
+        for word in sentence.word_list:
+            a_value = a / (a + get_word_frequency(word, look_table))  # smooth inverse frequency, SIF
+            vs = np.add(vs, np.multiply(a_value, get_word_vector(word, word_vec)))  # vs += sif * word_vector
+        vs = np.divide(vs, sentence_length)  # weighted average
+        sentence_set.append(vs)  # add to our existing re-calculated set of sentences
+    if len(sentence_list) < 2:
+        return sentence_set
+    pca = PCA()
+    pca.fit(np.array(sentence_set))
+    u = pca.components_[0]  # the PCA vector
+    u = np.multiply(u, np.transpose(u))  # u x uT
+
+    if len(u) < word_vec.vector_size:
+        for i in range(word_vec.vector_size - len(u)):
+            u = np.append(u, 0)  # add needed extension for multiplication below
+
+    sentence_vecs = []
+    for vs in sentence_set:
+        sub = np.multiply(u, vs)
+        sentence_vecs.append(np.subtract(vs, sub))
+    return sentence_vecs
+
+
+def load_word_vector_model(path: str):
+    print("加载的词向量的路径: " + path)
+    # 加载glove转换的模型: 保存的为文本形式
+    word_embedding = KeyedVectors.load_word2vec_format(path)
+    return word_embedding
+
+
+def get_max_length_doc(path: str):
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        docs = str(lines).split("\\n")
+        max_length = 0
+        print(len(docs))
+        for doc in docs:
+            content = doc.split(" ")
+            if len(content) > max_length:
+                max_length = len(content)
+    return max_length
+
+
+def generate_sentence_vector(content: str, word_vec_model: Word2Vec):
+    words = content.strip(" ")
+    word_vec = np.zeros(word_vec_model.vector_size)
+    for word in words:
+        word_vec += get_word_vector(word, word_vec_model)
+    word_vec = word_vec / len(words)
+    return word_vec
+
+
+def load_docs(path: str):
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        docs = str(lines).split("\\n")
+    return docs
+
+
+# model = load_word_vector_model('./sgns.wiki.model')
+# print('加载完成。')
+# docs = load_docs('./other_news.txt')
+# for i in range(1, 10):
+#     vec = generate_sentence_vector(docs[i], model)
+#     print(vec)
+
+# TODO 数据集 划分训练集和测试集 数据贴标签
